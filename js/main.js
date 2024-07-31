741dee43738b29e326b1a179facb19178a7c437c
Vladimir Alekseev <vladimir.alekseev@gointeractive.co>
2024-08-01 02:16:18 +0300

    Hotel detail page

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
+let perPageCount = function () {
+    let perPage = $(window).width() > 1383 ? 4 : 3;
+    if ($(window).width() < 974) {
+        perPage = 2;
+    }
+    if ($(window).width() < 750) {
+        perPage = 1;
+    }
+    return perPage;
+}
+
 $('#menu-up-control').click(function () {
-    let menuGeneral = $('#menu-general');
     if ($(this).hasClass('menu-up-control-is-close')) {
         $(this).removeClass('menu-up-control-is-close');
         $(this).addClass('menu-up-control-is-open');
-        menuGeneral.removeClass('menu-general-control-is-close');
-        menuGeneral.addClass('menu-general-control-is-open');
         $('header').addClass('header-menu-is-open');
     } else {
         $(this).removeClass('menu-up-control-is-open');
         $(this).addClass('menu-up-control-is-close');
-        menuGeneral.removeClass('menu-general-control-is-open');
-        menuGeneral.addClass('menu-general-control-is-close');
         $('header').removeClass('header-menu-is-open');
     }
 });
